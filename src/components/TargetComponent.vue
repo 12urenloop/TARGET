40599b8cebec412c1a6ad5394ad93ac07a523cce
smessie <smessie@smessie.com>
2023-02-20 22:10:30 +0100

    feat: Make teams amount variable

--- src/components/TargetComponent.vue
+++ src/components/TargetComponent.vue
@@ -3,21 +3,24 @@
     <h1>TARGET</h1>
     <h2>Top-down Advancement of the Running Groups Estimation Technology</h2>
     <div class="track-outer">
-      <div v-for="i in 20" :key="i">
+      <div v-for="i in teams.length" :key="i">
         <div
           class="track"
           :style="
             'height: ' +
-            (400 - i * 11) +
+            (400 - i * (220 / teams.length)) +
             'px; width: ' +
-            (900 - i * 11) +
+            (900 - i * (220 / teams.length)) +
             'px; border-radius: ' +
-            (200 - i * 5) +
+            (200 - i * (100 / teams.length)) +
             'px; left: ' +
-            5.5 * i +
+            (110 / teams.length) * i +
             'px; top: ' +
-            5.5 * i +
-            'px'
+            (110 / teams.length) * i +
+            'px; border: ' +
+            100 / teams.length +
+            'px solid ' +
+            teams[i - 1].color
           "
         ></div>
       </div>
@@ -33,6 +36,93 @@ export default {
   components: {
     MDBContainer,
   },
+  data() {
+    return {
+      // 20 teams
+      teams: [
+        {
+          name: "Team 1",
+          color: "#b27569",
+        },
+        {
+          name: "Team 2",
+          color: "#825046",
+        },
+        {
+          name: "Team 3",
+          color: "#b27569",
+        },
+        {
+          name: "Team 4",
+          color: "#825046",
+        },
+        {
+          name: "Team 5",
+          color: "#b27569",
+        },
+        {
+          name: "Team 6",
+          color: "#825046",
+        },
+        {
+          name: "Team 7",
+          color: "#b27569",
+        },
+        {
+          name: "Team 8",
+          color: "#825046",
+        },
+        {
+          name: "Team 9",
+          color: "#b27569",
+        },
+        {
+          name: "Team 10",
+          color: "#825046",
+        },
+        {
+          name: "Team 11",
+          color: "#b27569",
+        },
+        {
+          name: "Team 12",
+          color: "#825046",
+        },
+        {
+          name: "Team 13",
+          color: "#b27569",
+        },
+        {
+          name: "Team 14",
+          color: "#825046",
+        },
+        {
+          name: "Team 15",
+          color: "#b27569",
+        },
+        {
+          name: "Team 16",
+          color: "#825046",
+        },
+        {
+          name: "Team 17",
+          color: "#b27569",
+        },
+        {
+          name: "Team 18",
+          color: "#825046",
+        },
+        {
+          name: "Team 19",
+          color: "#b27569",
+        },
+        {
+          name: "Team 20",
+          color: "#825046",
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -42,14 +132,16 @@ export default {
 }
 
 .track {
-  border: 5px solid #b27569;
   transition: all ease-in-out 200ms;
   position: absolute;
 }
+
 .track:hover {
-  border: 5px solid #825046;
+  border-color: #825046 !important;
+  opacity: 0.8;
   cursor: pointer;
 }
+
 .track:hover::before {
   background: #825046;
 }
