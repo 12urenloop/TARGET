ef74cde531390487ee312449aea494bc93c00eea
smessie <smessie@smessie.com>
2023-02-20 20:10:21 +0100

    feat: Create main component and remove others

---
+++ src/components/TargetComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <MDBContainer>
+    <h1>TARGET</h1>
+    <h2>Top-down Advancement of the Running Groups Estimation Technology</h2>
+  </MDBContainer>
+</template>
+
+<script>
+import { MDBContainer } from "mdb-vue-ui-kit";
+
+export default {
+  name: "TargetComponent",
+  components: {
+    MDBContainer,
+  },
+};
+</script>
+
+<style scoped></style>
