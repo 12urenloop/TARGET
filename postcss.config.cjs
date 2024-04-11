e7d72242c354966654529c5c9ce0aae9c6a15b19
smessie <smessie@smessie.com>
2024-04-11 22:48:51 +0200

    feat: Configure PurgeCSS

---
+++ postcss.config.cjs
@@ -0,0 +1,39 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = () => ({
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+        purgecss({
+          content: ['dist/index.html', 'dist/assets/*.js'],
+          css: ['dist/assets/*.css'],
+          safelist: [
+            /-(leave|enter|appear)(|-(to|from|active))$/,
+            /^col-/,
+            /^offset-/,
+            /^btn-/,
+            /^badge-/,
+            /^border-/,
+            /^bg-/,
+            /^shadow-/,
+            /^border/,
+            /^text/,
+            /^list-group-/,
+            /^input-group-/,
+            /^flag-/,
+            /^fa-/,
+            /^table-/,
+            /^form-control-/,
+            /^row-cols-/,
+            /^container-/,
+            /^fade-height/,
+            /^alert-/,
+            /^toast-/,
+            /^chip-/,
+            /^sidenav-/,
+          ],
+          output: 'dist/assets',
+        }),
+      ]
+      : [],
+});
