4dc512ae22666fdce67171a91689b4524d16bf97
smessie <smessie@smessie.com>
2024-04-23 18:06:38 +0200

    feat: Add position visualisation on ledwall version

--- src/components/LedWallComponent.vue
+++ src/components/LedWallComponent.vue
@@ -4,6 +4,7 @@
     <div class="d-flex flex-wrap justify-content-center">
       <div class="team pb-1 pt-1 pb-2 pe-2" v-for="(team, index) in teams" :key="team.team_id">
         <MDBCard class="bg-dark">
+          <div class="card-position" :style="height(team)"></div>
           <MDBCardBody class="text-center d-flex flex-column align-items-center">
             <div class="rank text-white rounded-bottom rounded-4 rounded-start border-bottom border-start border-3">
               &nbsp;{{ index + 1}}&nbsp;
@@ -69,11 +70,42 @@ export default defineComponent({
     teams: Array<Team>,
     frozen: Boolean,
   },
+  data() {
+    return {
+      intervalId: 0,
+    };
+  },
   computed: {
     maxRounds() {
       return Math.max(...this.teams?.map(team => team.rounds) ?? [1]);
     },
   },
+  mounted() {
+    this.intervalId = setInterval(() => {
+      const now = Date.now();
+      this.teams?.forEach((team) => {
+        // Progress is in percentage of the path
+        // Speed is in percentage of the path per millisecond
+        // Acceleration is in percentage of the path per millisecond squared
+        team.speed += team.acceleration * (now - team.timestamp);
+        // Runners never go backwards
+        team.speed = Math.max(0, team.speed);
+        team.progress += team.speed * (now - team.timestamp);
+        team.timestamp = now;
+
+        // If the team is at the end of the path, reset it to the start
+        team.progress = team.progress % 1;
+      });
+    }, 100);
+  },
+  beforeUnmount() {
+    clearInterval(this.intervalId);
+  },
+  methods: {
+    height(team: Team) {
+      return `height: ${team.progress * 100}%`;
+    },
+  }
 });
 </script>
 
@@ -91,6 +123,7 @@ export default defineComponent({
   width: 100%;
   height: 100%;
   background-color: #000000;
+  z-index: 1;
 }
 
 h1 {
@@ -125,5 +158,18 @@ h1 {
   float: left;
   margin-top: -11em;
   margin-left: 2em;
+}
+
+.card-position {
+  position: absolute;
+  background-color: #2c2ca0;
+  border-radius: 0.5rem;
+  z-index: 2;
+  bottom: 0;
+  width: 100%;
+}
+
+.card-body {
+  z-index: 3;
 }
 </style>
