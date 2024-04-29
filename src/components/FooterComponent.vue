b6b413e0af729a5b1bf83ff67a720e0f7d0a1fea
smessie <smessie@smessie.com>
2024-04-29 23:47:27 +0200

    feat: Add footer
    (cherry picked from commit 79095b007511b70a4b9078633c7e3e688193f964)

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <MDBFooter :text="['center', 'lg-start']" class="pt-4">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      Made with
+      <MDBIcon icon="heart" iconStyle="fas" class="heart" />
+      by
+      <a class="zeus" href="https://zeus.gent/">Zeus WPI</a>
+      - View on <a class="text-dark" href="https://github.com/12urenloop/TARGET">GitHub
+      <MDBIcon icon="github" iconStyle="fab" />
+    </a>
+    </div>
+    <!-- Copyright -->
+  </MDBFooter>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { MDBFooter, MDBIcon } from 'mdb-vue-ui-kit';
+
+export default defineComponent({
+  name: 'FooterComponent',
+  components: { MDBFooter, MDBIcon },
+});
+</script>
+
+<style scoped>
+.heart {
+  color: #f14668;
+}
+
+.zeus {
+  color: #ff7f00;
+  font-weight: bold;
+}
+</style>
